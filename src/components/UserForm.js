5dd3e525ed6731f8528355d91990e93831d2c1f9
rednaxela700 <ohleg@windowslive.com>
2020-03-22 09:07:34 +0100

    created initial stage of UserForm component

---
+++ src/components/UserForm.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+
+export class UserForm extends Component{
+    state = {
+        step: 1
+
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+
+}
